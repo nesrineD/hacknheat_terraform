11812d53e28452ea27c4444f1064272fc2a6e80f
StephanMandl-Senacor <129524350+StephanMandl-Senacor@users.noreply.github.com>
2024-09-11 11:22:06 +0200

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -10,5 +10,5 @@
 # A data source allows Terraform to use information defined outside of Terraform, in this case our already exisiting bucket in the GCP project.
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/storage_bucket
 
-# TODO: Upload Readme.md to the bucket you fetched above with your data source. Use your name as a prefix for the storage bucket object.
+# TODO: Upload Readme.md to the bucket you fetched above with your data source. Use your name as a prefix for the storage bucket object resource.
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_object
